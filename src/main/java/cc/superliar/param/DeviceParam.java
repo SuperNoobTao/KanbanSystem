a8287997629bfb3e4309cfe57566056a72fdf80d
SuperNoobTao <387944558@qq.com>
2016-11-29 11:26:46 +0800

    update

--- src/main/java/cc/superliar/param/DeviceParam.java
+++ src/main/java/cc/superliar/param/DeviceParam.java
@@ -3,6 +3,8 @@ package cc.superliar.param;
 import cc.superliar.annotation.NotNullField;
 import cc.superliar.enums.OperationType;
 
+import javax.validation.constraints.Size;
+
 /**
  * Created by shentao on 2016/11/28.
  */
@@ -10,7 +12,78 @@ public class DeviceParam extends BaseParam{
 
     private static final long serialVersionUID = -9153801716112918626L;
 
-//    @NotNullField(value = {OperationType.CREATE,OperationType.UPDATE, OperationType.DELETE},)
+    @NotNullField(value = {OperationType.CREATE,OperationType.UPDATE, OperationType.DELETE}, message = "udid cannot be null.")
+    private String udid; // device's udid
 
+    @NotNullField(value = OperationType.CREATE, message = "name cannot be null.")
+    @Size(min = 4, max = 50)
+    private String name; // name
 
+    @NotNullField(value = OperationType.CREATE, message = "location cannot be null.")
+    @Size(min = 4, max = 50)
+    private String location; // location
+
+    @NotNullField(value = OperationType.CREATE, message = "size cannot be null.")
+    @Size(min = 4, max = 50)
+    private String size; // size
+
+    @NotNullField(value = OperationType.CREATE, message = "num cannot be null.")
+    @Size(min = 4, max = 50)
+    private String num; // num
+
+    private String description;
+
+    public DeviceParam() {}
+
+    public DeviceParam(String udid) {
+        this.udid = udid;
+    }
+
+    public String getUdid() {
+        return udid;
+    }
+
+    public void setUdid(String udid) {
+        this.udid = udid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
